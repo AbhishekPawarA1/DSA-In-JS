ec30a134d357f07be13524e531ee695bb4023ad7
AbhishekA1 <abhishekanandpawar1@gmail.com>
2025-01-20 00:06:14 +0530

    lon uniq substring

---
+++ string/Find the Longest Substring Without Repeating Characters.js
@@ -0,0 +1,37 @@
+// Find the Longest Substring Without Repeating Characters
+
+// Example:
+// Input: "abcabcbb"
+// Output: 3
+// Explanation: "abc".
+
+// Input: "bbbbb"
+// Output: 1
+// Explanation:"b".
+
+// Input: "pwwkew"
+// Output: 3
+// Explanation: "wke".
+
+
+let str = "bbbbb";
+let char_set = new Set()
+let left = 0
+let longest_sub = ""
+for (let right = 0; right < str.length; right++) {
+    while (char_set.has(str[right])) {
+        char_set.delete(str[left])
+        left+=1
+    }
+    char_set.add(str[right])
+    if (right - left + 1 > longest_sub.length) {
+        longest_sub=str.slice(left,right+1)
+    }
+}
+console.log(longest_sub);
+
+
+
+
+
+
